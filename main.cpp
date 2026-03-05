2faac4725448516e76b5cb6ab69b730140ddc29f
kirill <kirill@kirill.kirill>
2026-03-05 07:31:35 -0500

    add push_back + add push_front

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ struct BiList {
   T val;
   BiList<T>* next;
   BiList<T>* prev;
+  void push_back(T data);
+  void push_front(T data);
 };
 
 template<class T>
@@ -13,15 +15,43 @@ void clear(BiList<T>* list) {
     list = list->prev;
   }
   while (list != nullptr) {
-    BiLIst<T>* next = list->next;
+    BiList<T>* next = list->next;
     delete list;
     list = next;
   }
 }
 
 template<class T>
-BiList<T> operator=(BiList)
+void BiList<T>::push_back(T data) {
+  BiList<T>* change = this;
+  while (change->next != nullptr) {
+    change = change->next;
+  }
+  change->next = new BiList<T>;
+  change->next->prev = change;
+  change->next->val = data;
+  change->next->next = nullptr;
+}
+
+template<class T>
+void BiList<T>::push_front(T data) {
+  BiList<T>* change = this;
+  while (change->prev != nullptr) {
+    change = change->prev;
+  }
+  change->prev = new BiList<T>;
+  change->prev->val = data;
+  change->prev->prev = nullptr;
+  change->prev->next = change;
+}
 
 int main() {
-
+  BiList<int> q{0, nullptr, nullptr};
+  q.push_back(3);
+  q.push_front(7);
+  BiList<int>* c = &q;
+  while (c != nullptr) {
+    std::cout << c->val << std::endl;
+    c = c->next;
+  }
 }
